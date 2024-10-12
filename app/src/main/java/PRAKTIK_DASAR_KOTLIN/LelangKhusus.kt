7ee98109a6815d566a3431001eb4285176b37e54
ramizahhbudi <ramizahbudicp@gmail.com>
2024-10-12 23:28:09 +0700

    praktik dasar kotlin pertemuan 6

---
+++ app/src/main/java/PRAKTIK_DASAR_KOTLIN/LelangKhusus.kt
@@ -0,0 +1,2 @@
+package PRAKTIK_DASAR_KOTLIN
+
---
+++ app/src/main/java/PRAKTIK_DASAR_KOTLIN/notifikasi.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val morningNotification = 51
+    val eveningNotification = 135
+
+    printNotificationSummary(morningNotification)
+    printNotificationSummary(eveningNotification)
+}
+
+fun printNotificationSummary(numberOfMessages: Int) {
+    if (numberOfMessages < 100) {
+        println("Anda mempunyai ${numberOfMessages} notifikasi.")
+    } else {
+        println("Ponsel Anda berdering! Anda memiliki 99+ notifikasi.")
+    }
+}
